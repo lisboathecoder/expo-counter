66f991257ed67df176463e7e294842134556cd78
lisboathecoder <gustavo.lisboa2009@gmail.com>
2026-02-27 11:22:10 -0300

    :tada: first commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => {setContador(contador + 1);
+  };
+
+  const decrementar = () => {setContador(contador - 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.numero}>{contador}</Text>
+        <View style={styles.linhaDeBotoes}>
+          <TouchableOpacity style={styles.botaoMais} onPress={incrementar}> + </TouchableOpacity>
+          <TouchableOpacity style={styles.botaoMenos} onPress={decrementar}> - </TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -17,4 +28,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  numero: {
+    fontSize: 80, 
+    fontWeight: 'bold',
+    color: '#000000',
+  },
+  linhaDeBotoes: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  botaoMais: {
+    padding: 20,
+    backgroundColor: '#4CAF50',
+  },
+  botaoMenos: {
+    padding: 20,
+    backgroundColor: '#F44336',
+  },
+
 });
